85d8fc99beb43da663f094ffb2db9f5996971b8b
Lorenna Laieny <lorennalaieny@outlook.com>
2025-08-31 21:51:01 -0300

    dicionarios

---
+++ atividade2.py
@@ -0,0 +1,6 @@
+nota = int(input("Digite sua nota:"))
+
+if nota >= 7:
+    print("aprovado")
+elif nota < 7:
+    print("reprovado")
---
+++ atividade3.py
@@ -0,0 +1,8 @@
+numero = int(input("Digite um numero: "))
+number = int(input("Digite outro numero: "))
+
+if numero > number:
+    print(numero, "é maior que", number)
+    
+elif numero < number:
+   print(numero, "é menor que", number)
